4a1dc16afb7845c71c09b1e84d24e2ca8014bb2e
Utsav <161831513+Utsav631@users.noreply.github.com>
2024-09-01 03:13:47 +0530

    by utsav

--- routes/analysis.js
+++ routes/analysis.js
@@ -4,25 +4,19 @@ const Post = require('../models/form.models.js');
 
 const { countByType, countByLocation, countBySector } = require('../filter');
 
-
-// Route to fetch analysis data
 router.get('/', async (req, res) => {
   try {
-    const countByType = await countByType();
-    console.log('Count by type:', countByType);
-    const countByLocation = await countByLocation();
-    console.log('Count by locaation:', countByLocation);
-    const countBySector = await countBySector();
-    console.log('Count by sector:', countBySector);
-    
+    const typeCounts = await countByType();
+    const locationCounts = await countByLocation();
+    const sectorCounts = await countBySector();
+
     const result = {
-      countByType,
-      countByLocation,
-      countBySector}
-      ;
+      typeCounts,
+      locationCounts,
+      sectorCounts
+    };
 
     res.json(result);
-
   } catch (err) {
     console.error('Error fetching analysis data:', err);
     res.status(500).json({ error: 'Failed to fetch data' });
