2d00bd89b15a78cc155d89fca818443605409c99
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-09-01 02:19:47 +0530

    refactor: Remove unused code and update package.json scripts
    This commit removes unused code in the `index.js` file and updates the `package.json` scripts to use the `server.js` file instead of `index.js`. It improves code cleanliness and aligns the project with best practices.

--- routes/analysis.js
+++ routes/analysis.js
@@ -5,35 +5,6 @@ const Post = require('../models/form.models.js');
 const { countByType, countByLocation, countBySector } = require('../filter');
 
 
-// Route to fetch the latest 10 news articles
-router.get('/', async (req, res) => {
-    try {
-        // countByType()
-        //     .then(result => {
-        //         console.log('Count by type:', result);
-        //     })
-        //     .catch(err => console.error(err));
-
-        // Example: Get count by location
-        // countByLocation()
-        //     .then(result => {
-        //         console.log('Count by location:', result);
-        //     })
-        //     .catch(err => console.error(err));
-
-        // Example: Get count by sector
-        countBySector()
-            .then(result => {
-                console.log('Count by sector:', result);
-            })
-            .catch(err => console.error(err));
-
-    } catch (err) { }
-});
-
-module.exports = router;
-
-
 // Route to fetch analysis data
 router.get('/', async (req, res) => {
   try {
