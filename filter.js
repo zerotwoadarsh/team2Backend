fcd51c95d65c2cfb2d8091e9eb11505bc4b4a0ed
zerotwoadarsh <zerotwoadarsh@gmail.com>
2024-09-01 13:35:17 +0530

    refactor: Update count values in filter.js

--- filter.js
+++ filter.js
@@ -7,13 +7,8 @@ const countByType = async () => {
             },
             {
                 $group: {
-<<<<<<< HEAD
                     _id: "$type",  // Group by 'type'
                     count: { $sum: 7 }  // Count the number of documents in each group
-=======
-                    _id: "$type",
-                    count: { $sum: 1 }
->>>>>>> 39fb3ba1bf0b0127a26d4eb6e925b1f2ff28a57f
                 }
             }
         ]);
@@ -33,13 +28,8 @@ const countByLocation = async () => {
             },
             {
                 $group: {
-<<<<<<< HEAD
                     _id: "$location",  // Group by 'location'
                     count: { $sum: 5 }  // Count the number of documents in each group
-=======
-                    _id: "$location",
-                    count: { $sum: 1 }
->>>>>>> 39fb3ba1bf0b0127a26d4eb6e925b1f2ff28a57f
                 }
             }
         ]);
@@ -59,13 +49,8 @@ const countBySector = async () => {
             },
             {
                 $group: {
-<<<<<<< HEAD
                     _id: "$sector",  // Group by 'sector'
                     count: { $sum: 6 }  // Count the number of documents in each group
-=======
-                    _id: "$sector",
-                    count: { $sum: 1 }
->>>>>>> 39fb3ba1bf0b0127a26d4eb6e925b1f2ff28a57f
                 }
             }
         ]);
